1c199396cede4354b006954a20eefb8432a0e0ed
Yunitrish <yunthomas006@gmail.com>
2026-02-13 12:53:23 +0800

    更新 mod 版本至 1.1.0，新增 DiscordBridge 類別以實現 Minecraft 聊天訊息與 Discord 的橋接功能

--- src/main/java/com/adaptor/deadrecall/Deadrecall.java
+++ src/main/java/com/adaptor/deadrecall/Deadrecall.java
@@ -2,6 +2,8 @@ package com.adaptor.deadrecall;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -9,10 +11,24 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Deadrecall implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("DeadRecall");
+
     @Override
     public void onInitialize() {
+        // 初始化 Discord 橋接
+        DiscordBridge.init(FabricLoader.getInstance().getConfigDir());
+
+        // 監聽玩家聊天訊息，轉發到 Discord
+        ServerMessageEvents.CHAT_MESSAGE.register((message, sender, params) -> {
+            String username = sender.getName().getString();
+            String content = message.getContent().getString();
+            DiscordBridge.sendChatMessage(username, content);
+        });
+
         // 僅註冊 /back 指令，死亡座標記錄交由 Mixin 處理
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(CommandManager.literal("back")
